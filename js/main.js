df85ed9b89b1c07f785e15e643a6cea51d151054
Myroslav-Dmukhivskyi <mdmukhivskyi@gmail.com>
2021-07-22 11:22:37 +0300

    Full version

--- js/main.js
+++ js/main.js
@@ -9,19 +9,21 @@ $(function () {
       '<div class="prev-arrow"><img src="./images/prev-arrow.svg" alt="previous"></div>',
     nextArrow:
       '<div class="next-arrow"><img src="./images/next-arrow.svg" alt="next"></div>',
-    // responsive: [
-    //     {
-    //       breakpoint: 841,
-    //       settings: {
-    //         slidesToShow: 2
-    //       }
-    //     },
-    //     {
-    //       breakpoint: 501,
-    //       settings: {
-    //         slidesToShow: 1
-    //       }
-    //     }
-    //   ]
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 575,
+          settings: {
+            arrows:false,
+            dots: true,
+            slidesToShow: 1
+          }
+        }
+      ]
   });
 });
